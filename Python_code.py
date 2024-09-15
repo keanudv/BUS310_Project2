cc8c74d8d5e41b1ea7ae81d7be19628bc4fe4527
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-09-14 19:47:48 -1000

    Update Python_code.py

--- Python_code.py
+++ Python_code.py
@@ -26,7 +26,7 @@ clean_df.isnull().sum()
 # Select the needed columns
 columns = [
   "City",
-  "Land_SQFT",
+  "Living_SQFT",
   "Sold_Price"
 ]
 
@@ -42,12 +42,30 @@ lahaina_df.head(10)
 lahaina_df.tail(10)
 
 # Create the scatterplot with a trend line
-sns.regplot(data=lahaina_df, x="Land_SQFT", y="Sold_Price", color="red")
+sns.regplot(data=lahaina_df, x="Living_SQFT", y="Sold_Price", color="red")
 plt.title("Square Foot vs. Price", fontsize=30)
 plt.xlabel("Square Foot (In Millions)", fontsize=20)
 plt.ylabel("Sold Price (In Millions)", fontsize=20)
 plt.show()
 
 # Calculate the Correlation Coefficient
-correl = lahaina_df["Land_SQFT"].corr(lahaina_df["Sold_Price"])
 print(correl)
+
+'''
+There are a few outliers that we need to drop. To do this, we can use the Interquartile Range (IQR) method:
+  Step 1: Calculate the IQR.
+  Step 2: Define the boundaries.
+  Step 3: Drop the outliers.
+'''
+
+# Step 1: Calculate the IQR for the sold price
+Q1 = lahaina_df["Sold_Price"].quantile(0.25)
+Q3 = lahaina_df["Sold_Price"].quantile(0.75)
+IQR = Q3 - Q1
+
+# Step 2: Define the outlier boundaries
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Step 3: Drop the outliers
+filtered_df = lahaina_df[(lahaina_df["Sold_Price"] >= lower_bound) & (lahaina_df["Sold_Price"] <= upper_bound)]
